7eea998095cb604f263b85a323ac47fa58f4535d
Ramith2004 <ramithstewart2004@gmail.com>
2023-12-19 20:44:51 +0530

    new file about variables and constants

---
+++ var&const.js
@@ -0,0 +1,12 @@
+const accountId = 15647//constants
+let email = "Ramith@gmail.com"
+let password = "Ramith2345"// var and let are of same use so avoid using var due to some uses problem prefer to use let 
+// accountId = 158755
+let accState
+email="hct@gmail.com"
+password = "123456"
+console.log(accountId);
+console.log(email);
+console.log(password);
+console.log(accState);
+console.table([accountId,email,password,accState])
