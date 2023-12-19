939654e7a6fb194fd1aa1315cb54837d261acb34
Ramith2004 <ramithstewart2004@gmail.com>
2023-12-19 22:29:46 +0530

    all about datatypes

---
+++ datatype.js
@@ -0,0 +1,22 @@
+let name = "Ramith"
+var state= null
+let age = 18
+let isLoggedin = true
+// number => without " "
+//string => " "
+// boolean +> {true/false}
+// null => standalone value
+// undefined => when you donot assign some value to the varible
+console.log(name);
+console.log(state);
+console.log(age);
+console.log(isLoggedin);
+
+/*all the above are premetive
+but ther are others are also like derived
+*/
+
+
+// object
+console.log(typeof state);  
+
