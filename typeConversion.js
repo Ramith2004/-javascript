024b89bbf76974bebdb51616e24715d77564b3f5
Ramith2004 <147105205+Ramith2004@users.noreply.github.com>
2024-01-24 18:22:03 +0530

    CONVERT ONE TYPE TO ANOTHER

---
+++ typeConversion.js
@@ -0,0 +1,9 @@
+ let value = "33"
+ console.log(value);
+ console.log(typeof value);
+ let score = Number(value)
+ console.log(score);
+ console.log(typeof score);
+// Number String
+// If mixture of numver and alphabet is given then NaN will be displayed
+// If null is given then 0 will be displayed
